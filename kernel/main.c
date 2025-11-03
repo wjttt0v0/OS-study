05dd277fb94702f13ac81dea04221267d3cd9fa9
wjt <642028116@qq.com>
2025-11-03 11:15:35 +0800

    update stack

--- kernel/main.c
+++ kernel/main.c
@@ -1,5 +1,11 @@
 #include "types.h"
+#include "param.h"
 #include "defs.h"
+
+// Per-CPU stacks for entry.S.
+// NCPU is defined in param.h.
+// This array will be placed in the .bss section by the linker.
+__attribute__ ((aligned (16))) char stack0[4096 * NCPU];
 
 void main(void) {
     consoleinit();
